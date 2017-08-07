62b2c7585621af0f9c89696e37621f5fbfc3505d
Arsedian Ivanurrahman <arsedian.ivan@gmail.com>
2017-08-07 11:54:19 +1000

    Week 3 Final Commit

--- week3/server.js
+++ week3/server.js
@@ -1,9 +1,23 @@
  var http = require('http');
- var port = 3000;
+ var port = 3050;
+ var fs = require('fs');
 
- var requestHandler = function(request, response){
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        console.log("Request came from: " + request.url);
+        response.end();  
+    }).listen(port);
+});
+
+
+ /*var requestHandler = function(request, response){
      console.log("Request came from: " + request.url);
-     response.end("Hello, World! Welcome to 32549 Advanced Internet Programming Week 3 by Arsedian Ivanurrahman ");
+     response.end("Hello, World! Welcome to 32549 Advanced Internet Programming Week 3 by Arsedian Ivanurrahman on 07/07/2017 ");
  }
 
  var server = http.createServer(requestHandler);
@@ -11,3 +25,4 @@
  server.listen(port, function(){
      console.log("Listening on port " + port);
  });
+ */
